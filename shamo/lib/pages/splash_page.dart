15ad1fd67062d4c93503348c8f4ff71d59b3dd8b
danyriansyah <danyriansyahp@gmail.com>
2021-11-08 19:00:28 +0700

    update splash page

--- shamo/lib/pages/splash_page.dart
+++ shamo/lib/pages/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -6,11 +7,18 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor1,
       body: Center(
-        child: Text(
-          'Splash Page',
-        ),
-      ),
+          child: Container(
+        width: 130,
+        height: 150,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage(
+            'assets/image_splash.png',
+          ),
+        )),
+      )),
     );
   }
 }
