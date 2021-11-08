8a0b8d04a39413c8777282f5f68e3ecbe2128b35
danyriansyah <danyriansyahp@gmail.com>
2021-11-08 18:34:07 +0700

    costum font google

--- shamo/lib/main.dart
+++ shamo/lib/main.dart
@@ -20,44 +20,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset(
-            'assets/image_splash.png',
-          ),
-        ],
+      body: Image.asset(
+        'assets/image_splash.png',
       ),
     );
   }
 }
-
-
-
-// import 'package:flutter/material.dart';
-
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: HomePage(),
-//     );
-//   }
-// }
-
-// class HomePage extends StatelessWidget {
-//   const HomePage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Image.asset(
-//         'assets/image_splash.png',
-//       ),
-//     );
-//   }
-// }
