971769bcaa66023114b9591c2df32e47e6710f5d
danyriansyah <danyriansyahp@gmail.com>
2021-11-09 14:40:03 +0700

    add sign in page and route

--- shamo/lib/main.dart
+++ shamo/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shamo/pages/sing_in_page.dart';
 import 'package:shamo/pages/splash_page.dart';
 
 void main() => runApp(MyApp());
@@ -11,7 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      routes: {
+        '/': (context) => SplashPage(),
+        '/sign-in': (context) => SignInPage(),
+      },
     );
   }
 }
--- shamo/lib/pages/splash_page.dart
+++ shamo/lib/pages/splash_page.dart
@@ -1,8 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.pushNamed(context, '/sign-in'),
+    );
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
