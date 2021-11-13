d8488e56cff861bbdc95fc6c01b618437fc5f5fb
danyriansyah <danyriansyahp@gmail.com>
2021-11-13 14:07:03 +0700

    slicing sign in page

--- shamo/lib/pages/sing_in_page.dart
+++ shamo/lib/pages/sing_in_page.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Column(
+          children: [
+            Text(
+              'Login',
+              style: primaryTextStyle.copyWith(
+                fontSize: 24,
+                fontWeight: semiBold,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Sign In Page',
+      backgroundColor: backgroundColor1,
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          child: Column(
+            children: [
+              header(),
+            ],
+          ),
         ),
       ),
     );
